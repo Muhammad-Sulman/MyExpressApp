0c882d310cb93f9d70a6c2a8cf4206edce6b892f
M.Suleman <muhammadsulemanm431@gmail.com>
2025-01-31 16:12:14 +0500

    Routing

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ app.get('/', (req, res) => {
     res.send("Hello World..! iam avenger");
 });
 
+app.get('/about', (req, res) => {
+    res.send("Hello World..! iam About Page");
+});
+
+app.get('/contact', (req, res) => {
+    res.send("Hello World..! iam Contact Page");
+});
+
+app.get('/Search', (req, res) => {
+    console.log("Data Sent By Client...: ", req.query);
+    res.send("Hello World..! iam Search: "+ req.query.name);
+});
+
+app.get('*', (req, res) => {
+    res.send("404 Not Found Error...!");
+});
+
 app.listen(3000);
